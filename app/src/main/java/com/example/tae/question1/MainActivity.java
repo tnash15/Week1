4ff4e0a6d41213835c357d837bb05238b196d79b
tnash15 <tnash15@hotmail.com>
2018-12-07 18:59:40 +0000

    work commit

---
+++ app/src/main/java/com/example/tae/question1/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.tae.question1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Scanner;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+   public static class Palindrome {
+
+        public static void main(String args[]) {
+            String original, reverse="";
+            Scanner in = new Scanner(System. in);
+            System.out.print("Enter a string to check if it is a palindrome " );
+        }
+    }
+}
